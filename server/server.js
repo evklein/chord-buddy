02cff49ff066230e67b7fcaa36d27383d3f2d8e7
evanklein@bitbucket.com <evanklein@spensatech.com>
2018-11-20 11:19:52 -0500

    More configurations work.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
-import DATABASE_CREDENTIALS from 'database.config';
+// Gitignored in repo. To add your own credentials, simply add a 'db-config.js' file
+// in the server directory with the format:
+// module.exports = { host: '***', user: '***', password: '***', database: '***' };
+const DB_CONFIG = require('./db-config.js');
+const SERVER_CONFIG = require('./server-config.js'); // Same as above.
 
 const express = require('express');
 
 const app = express();
 
-app.route('/api/cats').get((req, res) => {
-	res.send({
-		cats: [{ name: 'lilly'  }, { name: 'lucy' }]
-	});
+app.listen(SERVER_CONFIG.port, () => {
+	console.log('Server started...');
+	console.log(DB_CONFIG);
 });
 
-app.listen(8000, () => {
-	console.log('Server started...');
-	console.log(DATABASE_CREDENTIALS);
-});
 
